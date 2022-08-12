866d9d37618d7f9e6904dda33c3d2095a41d0fe8
samira <samiraabdiurmia@gmail.com>
2022-08-12 14:36:08 -0700

    feat (KTH) : Add Article Footer style

---
+++ src/Styles/ArticleFooter/index.tsx
@@ -0,0 +1,35 @@
+export const linkStyle={
+    fontSize: "1rem",
+    fontHeight: "400",
+    color: "#006cb7",
+    textDecoration: "none",
+    backgroundColor: "transparent",
+    marginBottom: "14px",
+    lineHeight: "1.5",
+    display:"inline-block",
+    "& .linkColor" :{
+        color: "#006cb7",
+    }
+}
+
+
+export const NameStyle={
+    
+    fontSize: "1rem",
+    fontWeight: "600",
+    color: "#000",
+    lineHeight: "1.5",
+    marginTop: "0",
+    marginBottom: "14px",
+    display:  "inline-block",
+    marginRight: "4px",
+}
+export const TextStyle={
+    fontSize: "1rem",
+    fontWeight: "300",
+    color: "#000",
+    lineHeight: "1.5",
+    marginTop: "0",
+    marginBottom: "14px",
+    display:   "inline-block",
+}
