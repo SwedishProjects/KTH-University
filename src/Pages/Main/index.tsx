dc211ad399fa76354501b4091c24aac761ba886c
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2022-09-03 14:05:33 +0430

    fix bugs in student pages/main/index.tsx route

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -6,4 +6,4 @@ export { default as Library } from "./Library/Library";
 export { default as Research } from "./Research/Research";
 export { default as Studies } from "./Studies/Studies";
 export { default as Alumni } from "./Alumni/Alumni";
-// export { default as Student } from "./Student/Student"
+export { default as Student } from "./Student/Student"
