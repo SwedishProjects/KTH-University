ce8b15a62089f66b175889960eec5009d60d918f
samira <samiraabdiurmia@gmail.com>
2022-09-03 11:55:58 -0700

    feat (Login) : add Login page

---
+++ src/Components/Login/Login.tsx
@@ -0,0 +1,22 @@
+
+import { CardContent,
+ 
+    
+     Box,
+  
+    } from '@mui/material';
+
+    import {colorStyle ,} from "../../Styles/Login"
+function Login (){
+    return (
+        <>
+{/* <Box sx={colorStyle}>
+responsive 
+</Box> */}
+        </>
+    )
+}
+
+
+
+export default Login;
---
+++ src/Styles/Login/index.tsx
@@ -0,0 +1,12 @@
+
+export const colorStyle={
+    // height:"500px",
+    // margin:"0 15px",
+    // width:"100%",
+    // backgroundColor:{xs:"#3ea605",sm:"#edcc00",md:"#c1e3fa",lg:"#107abe"},
+    // fontSize:{xs:"10rem",sm:"8rem",md:"6rem",lg:"4rem"},
+    // border:false?"5px solid #000" :"6px solid #c1c1c1",
+    // display:"flex",
+    // justifyContent:"center",
+    // alignItems:"center",
+}
