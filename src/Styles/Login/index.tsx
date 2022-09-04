5afe0e533a49e8222aa95d7b1ee156df4dfa8026
samira <samiraabdiurmia@gmail.com>
2022-09-04 09:18:24 -0700

    feat (Login) : add Login style

--- src/Styles/Login/index.tsx
+++ src/Styles/Login/index.tsx
@@ -1,12 +1,313 @@
 
-export const colorStyle={
-    // height:"500px",
-    // margin:"0 15px",
-    // width:"100%",
-    // backgroundColor:{xs:"#3ea605",sm:"#edcc00",md:"#c1e3fa",lg:"#107abe"},
-    // fontSize:{xs:"10rem",sm:"8rem",md:"6rem",lg:"4rem"},
-    // border:false?"5px solid #000" :"6px solid #c1c1c1",
-    // display:"flex",
-    // justifyContent:"center",
-    // alignItems:"center",
+export const FullPage={
+ 
+        position:" absolute",
+        bottom: "0px",
+        top: "0px",
+        width:" 100%",
+        margin: "0px",
+        padding:" 0px",
+    
+  
 }
+export const loginText={
+    marginBottom: "0px",
+    marginTop:" 35px",
+    fontSize: "small",
+}
+
+export const brandingWrapper ={
+    backgroundColor:" #4488dd",
+    height: "100%",
+    width: "100%",
+    float:" left",
+    margin: "0px",
+    padding:" 0px",
+}
+
+
+export const branding ={
+    backgroundColor: "inherit",
+    backgroundRepeat: "noRepeat",
+    backgroundSize: "cover",
+    height: "100%",
+    width: "100%",
+   
+    margin: "0px",
+    padding: "0px",
+
+
+}
+export const brandingTint ={
+    backgroundColor:" rgba(0,0,0,0.4)",
+    height:" 100%",
+    width: "100%",
+    // -webkitBackgroundSize: cover,
+    // -mozBackgroundSize: cover,
+    // -oBackgroundSize: cover",
+}
+
+export const contentWrapper= {
+    backgroundColor:" transparent",
+    height:" 0px",
+    width: "100%",
+    float: "left",
+    margin:" 0px",
+    padding:" 0px",
+}
+
+export const content  ={
+    position:" fixed",
+    top: "50%",
+    left: "50%",
+    transform:" translate(-50%, -50%)",
+    backgroundColor:" #fff",
+    marginBottom: "28px",
+    marginLeft:" auto",
+    marginRight:" auto",
+    minHeight: "235px",
+    minWidth: "320px",
+    maxWidth:" 412px",
+    margin:" 0px",
+    width: "338px",
+    height: "auto",
+    padding: "36px",
+    boxShadow:" 0 2px 3px rgb(0 0 0 / 55%)",
+    border: "1px solid rgba(0,0,0,0.4)",
+}
+
+
+export const header ={
+    fontSize: "2em",
+    fontWeight: "lighter",
+    fontFamily: "'Segoe UI Webfont',AppleSystem,'Helvetica Neue','Lucida Grande','Roboto','Ebrima','Nirmala UI','Gadugi','Segoe Xbox Symbol','Segoe UI Symbol','Meiryo UI','Khmer UI','Tunga','Lao UI','Raavi','Iskoola Pota','Latha','Leelawadee','Microsoft YaHei UI','Microsoft JhengHei UI','Malgun Gothic','Estrangelo Edessa','Microsoft Himalaya','Microsoft New Tai Lue','Microsoft PhagsPa','Microsoft Tai Le','Microsoft Yi Baiti','Mongolian Baiti','MV Boli','Myanmar Text','Cambria Math'",
+    padding:" 0px 0px 0px 0px",
+    margin: "0px",
+    height: "36px",
+    width: "338px",
+    backgroundColor: "transparent",
+    wordWrap:" breakWord",
+}
+
+
+export const LogoImg ={
+    width: "auto",
+    height:" 70px",
+    position:" relative",
+    top: "-7px",
+}
+
+// ----------------------------main --------------------------
+
+
+export const workArea ={
+    marginBottom: "4%",
+    marginTop: "16px",
+    backgroundColor: "transparent",
+    margin: "0px",
+    padding:" 0px",
+}
+
+export const workAreaHeader ={
+    wordWrap:" break-word",
+}
+
+export const groupMargin ={
+    marginBottom:" 0px",
+    marginTop: "35px",
+    fontSize:" small",
+    margin:" 0px",
+    padding: "0px",
+}
+export const loginForm ={
+    width: "338px",
+    margin: "0px",
+    padding: "0px",
+}
+
+export const error ={
+    fontSize: "0.9em",
+    fontWeight: "normal",
+    marginBottom: "8px",
+    color:" #e81123",
+    margin:" 0px",
+    padding:" 0px",
+}
+// #password
+// #userNameArea,
+ export const Area ={
+    boxSizing: "border-box",
+    color:" rgb(38, 38, 38)",
+    direction:" ltr",
+    display:" block",
+    fontFamily: "Segoe UI Webfont', -appleSystem, 'Helvetica Neue', 'Lucida Grande', Roboto, Ebrima, 'Nirmala UI', Gadugi, 'Segoe Xbox Symbol', 'Segoe UI Symbol', 'Meiryo UI', 'Khmer UI', Tunga, 'Lao UI', Raavi, 'Iskoola Pota', Latha, Leelawadee, 'Microsoft YaHei UI', 'Microsoft JhengHei UI', 'Malgun Gothic', 'Estrangelo Edessa', 'Microsoft Himalaya', 'Microsoft New Tai Lue', 'Microsoft PhagsPa', 'Microsoft Tai Le', 'Microsoft Yi Baiti', 'Mongolian Baiti', 'MV Boli', 'Myanmar Text', 'Cambria Math'",
+    fontSize: "15px",
+    fontWeight:" normal",
+    height: "52px",
+    lineHeight:" 20px",
+    marginLeft: "0px",
+    marginRight: "-2px",
+    textAlign: "left",
+    textSizeAdjust: "100%",
+    width: "342px",
+}
+
+export const userNameLabel={
+    marginBottom:" 8px",
+}
+
+export const inputText = {
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    backgroundImage:" none",
+    borderBottomColor:" rgba(0, 0, 0, 0.6)",
+    borderBottomStyle: "solid",
+    borderBottomWidth: "1px",
+    // borderImage-outset: "0px",
+    // borderImageRepeat:" stretch",
+    // borderImageSlice: "100%",
+    // borderImageSource: "none",
+    // borderImageWidth: "1",
+    borderTop:" none",
+    borderLeft:" none",
+    borderRight: "none",
+    boxSizing:"border-box",
+    color: "rgb(38, 38, 38)",
+    cursor:" auto",
+    direction: "ltr",
+    display: "block",
+    // fontFamily: "Segoe UI Webfont", AppleSystem, "Helvetica Neue", "Lucida Grande", Roboto, Ebrima, "Nirmala UI", Gadugi, "Segoe Xbox Symbol", "Segoe UI Symbol", "Meiryo UI", "Khmer UI", Tunga, "Lao UI", Raavi, "Iskoola Pota", Latha, Leelawadee, "Microsoft YaHei UI", "Microsoft JhengHei UI", "Malgun Gothic", "Estrangelo Edessa", "Microsoft Himalaya", "Microsoft New Tai Lue", "Microsoft PhagsPa", "Microsoft Tai Le", "Microsoft Yi Baiti", "Mongolian Baiti", "MV Boli", "Myanmar Text", "Cambria Math"",
+    fontSize: "15px",
+    fontStretch:" normal",
+    fontStyle: "normal",
+    fontVariantCaps: "normal",
+    fontVariantLigatures: "normal",
+    fontVariantNumeric: "normal",
+    fontWeight:" normal",
+    height:"36px",
+    letterSpacing:" normal",
+    lineHeight: "20px",
+    marginBottom: "0px",
+    marginLeft: "0px",
+    marginRight:"0px",
+    marginTop:" 0px",
+    maxWidth: "100%",
+    outlineColor:" rgb(38, 38, 38)",
+    outlineStyle: "none",
+    outlineWidth: "0px",
+    paddingBottom: "6px",
+    paddingLeft:" 10px",
+    paddingRight: "10px",
+    paddingTop: "6px",
+    textAlign: "start",
+    textIndent: "0px",
+    textRendering:" auto",
+    textShadow: "none",
+    textSizeAdjust:" 100%",
+    textTransform:" none",
+    userSelect:" text",
+    width: "338px",
+    wordSpacing:" 0px",
+    writingMode:" horizontalTb",
+    // WebkitAppearance:" none",
+    // WebkitLocale: "en",
+    // WebkitRtl-ordering:" logical",
+    // WebkitBorderImage: "none",
+}
+
+
+
+
+   export const  fullWidth ={
+        width: "342px",
+    }
+
+
+  export const  inputCheckBox={
+        maxWidth:" 100%",
+        fontFamily: "inherit",
+        marginTop:" 0px",
+        verticalAlign:" middle",
+        marginBottom:" 0px",
+    }
+
+    export const submissionArea ={
+        marginTop: "8px",
+        marginBottom:" 18px",
+    }
+
+
+    export const submit={
+        alignItems: "flexStart",
+        backgroundColor: "rgb(0, 103, 184)",
+        borderBottomColor:" rgb(0, 103, 184)",
+        borderBottomStyle: "solid",
+        borderBottomWidth: "1px",
+        // borderImageOutset: "0px",
+        // borderImageRepeat: "stretch",
+        // borderImageSlice: "100%",
+        // borderImageSource: "none",
+        // borderImageWidth: "1",
+        borderLeftColor: "rgb(0, 103, 184)",
+        borderLeftStyle: "solid",
+        borderLeftWidth:" 1px",
+        borderRightColor: "rgb(0, 103, 184)",
+        borderRightStyle: "solid",
+        borderRightWidth: "1px",
+        borderTopColor: "rgb(0, 103, 184)",
+        borderTopStyle: "solid",
+        borderTopWidth: "1px",
+        boxSizing: "border-box",
+        color: "rgb(255, 255, 255)",
+        cursor:" pointer",
+        direction: "ltr",
+        display: "inlineBlock",
+        // fontFamily: "Segoe UI Webfont", AppleSystem, "Helvetica Neue", "Lucida Grande", Roboto, Ebrima, "Nirmala UI", Gadugi, "Segoe Xbox Symbol", "Segoe UI Symbol", "Meiryo UI", "Khmer UI", Tunga, "Lao UI", Raavi, "Iskoola Pota", Latha, Leelawadee, "Microsoft YaHei UI", "Microsoft JhengHei UI", "Malgun Gothic", "Estrangelo Edessa", "Microsoft Himalaya", "Microsoft New Tai Lue", "Microsoft PhagsPa", "Microsoft Tai Le", "Microsoft Yi Baiti", "Mongolian Baiti", "MV Boli", "Myanmar Text", "Cambria Math"",
+        fontSize:" 15px",
+        fontStretch: "normal",
+        fontStyle: "normal",
+        fontVariantCaps: "normal",
+        fontVariantLigatures: "normal",
+        fontVariantNumeric: "normal",
+        fontWeight: "normal",
+        height: "36px",
+        letterSpacing: "normal",
+        lineHeight: "25px",
+        marginBottom:"0px",
+        marginLeft:" 0px",
+        marginRight: "0px",
+        marginTop:" 0px",
+        maxWidth: "100%",
+        overflowX: "hidden",
+        overflowY: "hidden",
+        paddingBottom: "4px",
+        paddingLeft: "12px",
+        paddingRight: "12px",
+        paddingTop:" 3px",
+        position: "relative",
+        textAlign: "center",
+        textIndent: "0px",
+        textOverflow: "ellipsis",
+        textRendering:"auto",
+        textShadow: "none",
+        textSizeAdjust: "100%",
+        touchAction:" manipulation",
+        userSelect: "none",
+        verticalAlign:" middle",
+        whiteSpace: "nowrap",
+        width:" 100%",
+        wordSpacing:" 0px",
+        writingMode:" horizontalTb",
+        // WebkitAppearance:" none",
+        // WebkitRtl-ordering: "logical",
+        // WebkitBorderImage:" none",
+    }
+  export const   nextButton = {
+        marginLeft: "-2px",
+        left:" 229px",
+        bottom: "-30px",
+        height:" 32px",
+        width: "108px",
+    }
+
+  
+
