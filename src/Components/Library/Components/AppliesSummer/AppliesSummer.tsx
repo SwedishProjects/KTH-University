5f1d83215cb7a4c647dda2689ae29a00edc63876
samira <samiraabdiurmia@gmail.com>
2022-09-18 05:25:10 -0700

    feat (Library) : add AppliesSummer

--- src/Components/Library/Components/AppliesSummer/AppliesSummer.tsx
+++ src/Components/Library/Components/AppliesSummer/AppliesSummer.tsx
@@ -1,9 +1,44 @@
 import OpeningHours from "../OpeningHours/OpeningHours";
-
+import {  Container,Grid , } from "@mui/material";
+import ExploreCard from '../../../Explore/Components/ExploreCard/ExploreCard';
+import images from "../../../../Importer/Importer"
 function AppliesSummer(){
     return(
         <>
-        <OpeningHours />
+
+<Grid container spacing={2}>
+   
+   <Grid item xs={12} sm={6} md={6} lg={9}>
+   <Container
+      sx={{
+       
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        bgcolor: "#ffffff",
+        maxWidth: "100%",
+       width:"100%",
+      
+      padding:"0",
+        
+      }}
+    >
+       
+              <ExploreCard
+              title= "Summer time at the library"
+              image= {images['Library/sommar_biblioteket.jpg']}
+              Imagename= "Summer time at the library"
+              text = "Summer time is here, and that means that the library has limited opening hours and longer loan periods. However, our digital support is open all summer to answer your questions."
+              link="Read more about what applies at the library this summer"
+              />
+       
+    </Container>
+    </Grid>
+    <Grid item xs={12} sm={6} md={6} lg={3}>
+    <OpeningHours />
+    </Grid>
+    </Grid>
+      
         </>
     )
 }
